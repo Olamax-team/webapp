1bd1f3c2ca925b4a968f3b88d3c6ae92df694e26
Salomi Afolabi Onome <106906943+nomeo-310@users.noreply.github.com>
2024-12-03 12:05:29 +0100

    developer welcome message added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="flex items-center justify-center h-screen w-full">
       <div className='bg-lightBg p-4 rounded-md'>
-        <h1 className='text-secondary'>Hello Olamax!!!</h1>
+        <h1 className='text-secondary'>Hello Olamax Developer!!!</h1>
       </div>
     </div>
   )
