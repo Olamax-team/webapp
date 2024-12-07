9669cf5ddb52ee267e0078d94d81c23ee6d2cf99
Salomi Afolabi Onome <106906943+nomeo-310@users.noreply.github.com>
2024-12-07 01:54:20 +0100

    new changes made

--- frontend/src/components/navigation/BottomHeader.tsx
+++ frontend/src/components/navigation/BottomHeader.tsx
@@ -4,11 +4,14 @@ const BottomHeader = () => {
   return (
     <div className='w-full h-[100px]'>
       <div className="xl:w-[1440px] mx-auto border h-full xl:px-[95px] flex items-center">
+        
+        {/* home page link */}
         <Link to={'/about'}>
           <div className='w-[153px] h-[48px]'>
             <img src="../../../src/assets/images/olamax_logo_2.png" alt="logo" className='object-cover'/>
           </div>
         </Link>
+
       </div>
     </div>
   )
