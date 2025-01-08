08835c05456edbc4f06c7044a2922e390b18fc0c
Salomi Afolabi Onome <106906943+nomeo-310@users.noreply.github.com>
2025-01-08 19:36:33 +0100

    dashbord layout completed

--- frontend/src/components/dashboard/DashboardLayout.tsx
+++ frontend/src/components/dashboard/DashboardLayout.tsx
@@ -3,6 +3,7 @@ import { cn } from '../../lib/utils';
 import { HiOutlineCash, HiOutlineClipboardList, HiOutlineIdentification, HiOutlineUserCircle, HiOutlineGift, HiOutlineChartPie } from "react-icons/hi"
 import { IconType } from 'react-icons/lib';
 import { Link, useLocation } from 'react-router-dom';
+import { ChevronDown, ChevronUp } from 'lucide-react';
 
 type dashboardLayoutProps = {
   children: React.ReactNode;
@@ -10,9 +11,14 @@ type dashboardLayoutProps = {
 };
 
 type dashboardNavLinkProps = {
-  label: string;
-  icon: IconType;
-  path: string;
+  label?: string;
+  icon?: IconType;
+  path?: string;
+};
+
+type dashboardNavPlaceHolderProps = {
+  label?: string;
+  icon?: IconType;
 };
 
 const navLinkList = [
@@ -50,27 +56,88 @@ const navLinkList = [
 
 const DashboardLayout = ({children, className}:dashboardLayoutProps) => {
 
-  const DashboardNavLink = ({label, icon:Icon, path}:dashboardNavLinkProps) => {
+    const navbar = "bg-white w-full md:hidden fixed top-[96px] z-[4000]"
+    const navbarFixed = "bg-white w-full md:hidden fixed top-[64px] z-[4000]"
+
+    const [showMenu, setShowMenu] = React.useState(false);
+
+    const [navIsFixed, setNavIsFixed] = React.useState(navbar);
+
+    const toggleNavbarState = React.useCallback(() => {
+      if (window.scrollY >= 80) {
+        setNavIsFixed(navbarFixed);
+      } else {
+        setNavIsFixed(navbar);
+      }
+  
+    }, [navbar]);
+  
+    // for controlling it with useEffect
+    React.useEffect(() => {
+      toggleNavbarState();
+      
+      window.addEventListener('scroll', toggleNavbarState)
+    }, [toggleNavbarState]);
+
     const { pathname } = useLocation();
 
+   const DashboardNavLink = ({label, icon:Icon, path}:dashboardNavLinkProps) => {
+    
     return (
-      <Link to={path} className={cn("w-[290px] h-[60px] rounded-r-[30px] pl-[49px] flex item-center hover:bg-[#f5f5f5] group", pathname === path ? 'bg-[#f5f5f5]' : '')}>
+      <Link to={path ? path : ''} className={cn("md:w-[250px] lg:w-[280px] xl:w-[290px] h-[60px] rounded-r-[30px] pl-[20px] lg:pl-[40px] xl:pl-[49px] flex item-center hover:bg-[#f5f5f5] group", pathname === path ? 'bg-[#f5f5f5]' : '')}>
         <div className={cn('my-auto flex items-center gap-3 group-hover:text-primary', pathname === path ? 'text-primary': '')}>
-          <Icon className='size-6'/>
-          <p className='font-medium'>{label}</p>
+          {Icon && <Icon className='size-6'/>}
+          <p className='font-medium'>{label && label}</p>
         </div>
       </Link>
     );
   };
 
+  const MobileNavLink = ({label, icon:Icon, path}:dashboardNavLinkProps) => {
+    
+    return (
+      <Link to={path ? path : ''} className='flex gap-3 items-center group'>
+        {Icon && <Icon className={cn('size-6 group-hover:text-primary', path === pathname ? 'text-primary' : '')}/>}
+        <p className={cn('font-medium group-hover:text-primary', path === pathname ? 'text-primary' : '')}>{label}</p>    
+      </Link>
+    );
+  };
+
+  const PlaceHolder = ({label, icon:Icon}:dashboardNavPlaceHolderProps) => {
+
+    return (
+      <button className="flex items-center justify-between w-full font-poppins text-primary cursor-pointer" onClick={() => setShowMenu(!showMenu)}>
+        <div className='flex gap-3 items-center group'>
+          {Icon && <Icon className={cn('size-6')}/>}
+          <p className={cn('font-medium')}>{label && label}</p>    
+        </div>
+        { showMenu ? <ChevronDown/> : <ChevronUp/> }
+      </button>
+    )
+  }
+
   return (
-    <div className={cn('w-full flex', className)}>
-      <div className="w-[21%] sticky top-[100px] h-full py-10 font-poppins flex flex-col gap-8">
+    <div className={cn('w-full flex md:flex-row flex-col', className)}>
+      <div className="w-[33%] lg:w-[23%] xl:w-[22%] sticky md:top-[100px] h-full py-10 font-poppins hidden md:flex flex-col gap-8">
         { navLinkList.map((item) => (
           <DashboardNavLink {...item} key={item.path}/>
         ))}
       </div>
-      <div className="w-[79%] bg-[#f5f5f5]">
+      <div className={navIsFixed}>
+        <div className='h-[68px] flex items-center px-5 justify-between shadow shadow-[4px_4px_4px_0_rgba(0, 0, 0, 0.3)] border-t-2'>
+          <div className="flex gap-3 items-center w-full">
+            <PlaceHolder {...navLinkList.find(item => item.path === pathname)}/>
+          </div>
+        </div>
+        {showMenu &&
+          <div className='cursor-pointer absolute left-0 top-[68px] w-full p-6 border-t-2 bg-white gap-6 flex flex-col font-poppins shadow shadow-[4px_4px_4px_0_rgba(0, 0, 0, 0.3)]'>
+            {navLinkList.map((item, index:number) => (
+              <MobileNavLink {...item} key={index}/>
+            ))}
+          </div>
+        }
+      </div>
+      <div className="w-full md:w-[67%] lg:w-[77%] xl:w-[78%] bg-[#f5f5f5] pt-[64px] md:pt-0 ">
         {children}
       </div>
     </div>
