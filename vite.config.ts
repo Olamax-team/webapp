a26e1a7a5bf3a1564076a89f824e7e722e76c5c8
olamax-team <info@olamax.io>
2025-01-31 21:39:08 +0100

    Deploy build to gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,15 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import path from 'path'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-// })
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import path from 'path'
-
-// https://vite.dev/config/
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 export default defineConfig({
   plugins: [react()],
-  base: '/webapp/', // This must match your GitHub Pages repository name
-});
+  base: '/webapp/', // Make sure this matches the repository name or path in GitHub Pages
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: 'dist', // Make sure the build output is directed to 'dist'
+  },
+})
