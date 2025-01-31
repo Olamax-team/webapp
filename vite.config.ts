723d6c32acafb61dc0b71ad21f46758bb8d98daa
olamax-team <info@olamax.io>
2025-01-31 21:06:09 +0100

    Fixed build issues and deployed site

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,24 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import path from 'path'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/webapp/",  // Replace with your actual repository name
   plugins: [react()],
   resolve: {
     alias: {
