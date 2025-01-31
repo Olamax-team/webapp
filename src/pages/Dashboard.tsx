06b90e602ec9ebc8f9fa7b709b159e6b9f4f344b
olamax-team <info@olamax.io>
2025-01-31 21:01:28 +0100

    Resolved merge conflict in Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
-import DashboardLayout from '../components/dashboard/DashboardLayout'
-// import DashboardTab from '../components/dashboard/dashboardTabs'
-import DashboardTab from '../components/dashboard/DashboardTabs';
-=======
 import DashboardLayout from "../components/dashboard/DashboardLayout"
 import DashboardTab from "../components/dashboard/DashboardTabs"
 import { documentTitle } from "../lib/utils";
 
->>>>>>> a77aafe8d502a364c98e3f19debf4f748aa277f1
 
 
 const Dashboard = () => {
