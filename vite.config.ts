a65ccc797f3ca56bf8cb1ddaef31334c008572ce
olamax-team <info@olamax.io>
2025-01-31 21:19:04 +0100

    Fix build for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -12,17 +12,15 @@
 //   },
 // })
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import path from 'path'
 
 // https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-  base: "/webapp/",  // Replace with your actual repository name
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-})
+  base: '/webapp/', // This must match your GitHub Pages repository name
+});
