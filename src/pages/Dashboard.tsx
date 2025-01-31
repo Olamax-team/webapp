3522b049860eac5d26620837c52c428ac711c4bc
olamax-team <info@olamax.io>
2025-01-31 20:49:08 +0100

    Update package files

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,6 @@
 import DashboardLayout from '../components/dashboard/DashboardLayout'
-import DashboardTab from '../components/dashboard/dashboardTabs'
+// import DashboardTab from '../components/dashboard/dashboardTabs'
+import DashboardTab from '../components/dashboard/DashboardTabs';
 
 
 const Dashboard = () => {
