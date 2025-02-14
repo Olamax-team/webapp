6424fcf3c745fc5b53e4584010a05b952d00cc94
debyrise <deborahoby693@gmail.com>
2025-02-14 09:38:37 +0100

    about values

--- src/components/aboutUs/aboutUs.tsx
+++ src/components/aboutUs/aboutUs.tsx
@@ -11,7 +11,7 @@ const AboutUs = () => {
 
   
   return (
-    <section className="w-full h-auto p-2 lg:p-10 flex items-center justify-center flex-col text-center   ">
+    <section className="w-full h-auto p-4 lg:p-10 flex items-center justify-center flex-col text-center   ">
         <div className="lg:w-[60%] w-full text-center ">
             <h2 className="font-Inter font-bold text-[18px] leading-[27px] text-center text-[#121826] xl:font-DMSans xl:font-bold xl:text-[32px] xl:leading-[48px] ">We see no reason why buying & selling crypto should be so hard</h2>
             <p className="mt-5 font-Inter font-medium  text-[14px] text-center  text-[#121826] leading-[21px] xl:font-Inter xl:font-medium xl:leading-[27px] xl:text-[18px]"> 
@@ -26,9 +26,9 @@ const AboutUs = () => {
 
         <div>
           <div className=" lg:flex  justify-between  w-full h-auto  mt-10 gap-10 ">
-                <h2 className="xl:w-[50%] w-full font-DMSans font-bold text-[18px] leading-[27px] xl:text-left xl:px-5 text-center ">At <span className="text-[#039AE4] font-DMSans font-bold text-[18px] leading-[27px] items-center ">OLAMAX EXCHANGE</span>, we are the place where people reach their goals with cryptocurrencies and blockchain technology. 
+                <h2 className="xl:w-[50%]  w-full font-DMSans font-bold text-[18px] leading-[27px] xl:text-left  text-center ">At <span className="text-[#039AE4] font-DMSans font-bold text-[18px] leading-[27px] items-center ">OLAMAX EXCHANGE</span>, we are the place where people reach their goals with cryptocurrencies and blockchain technology. 
                 We do that by offering a full scope of secure solutions to buy, sell, and earn digital assets. </h2>
-                <p className="xl:w-[50%] w-full mt-10 xl:mt-0 font-Inter font-medium text-[14px] xl:text-left leading-[21px] xl:px-5 text-center ">Olamax.io is a product of Olamax associate  incorporated in Nigeria. We provide Cryptocurrency to Fiat exchange services for Bitcoin, Ethereum, Doge, Steem and Sbd to NAIRA. Our exchange provides you a marketplace
+                <p className="xl:w-[50%]  w-full mt-10 xl:mt-0 font-Inter font-medium text-[14px] xl:text-left leading-[21px] text-center ">Olamax.io is a product of Olamax associate  incorporated in Nigeria. We provide Cryptocurrency to Fiat exchange services for Bitcoin, Ethereum, Doge, Steem and Sbd to NAIRA. Our exchange provides you a marketplace
                   where you can buy and sell Cryptocurrencies easily and safely with notable simple UI, friendly online customer support 24/7 and lowest 
                   fees at the best rate you can't find anywhere else.
                   Our team comprises of professionals with extensive experience in financial products, E-currencies, 
@@ -52,7 +52,6 @@ const AboutUs = () => {
             </div>
         </div>
 
-        {/* <div> <OurValues/> </div>  */}
         <div> <OurValues/> </div>
     </section>
   )
--- src/components/aboutUs/ourValues.tsx
+++ src/components/aboutUs/ourValues.tsx
@@ -4,7 +4,7 @@ import { TbHeartHandshake } from "react-icons/tb";
 const OurValues = () => {
     return (
       <div className=" mt-10 w-full h-auto ">
-        <div className="text-left xl:text-center text-[#000000] p-3">
+        <div className="text-left xl:text-center text-[#000000] ">
           <h2 className="font-Inter font-bold text-[18px] leading-[27px] xl:font-DMSans xl:text-[32px] xl:leading-[48px]">
             Our Values
           </h2>
@@ -13,7 +13,7 @@ const OurValues = () => {
           </p>
         </div>
   
-        <div className="grid grid-cols-2 gap-2 xl:grid-cols-4 lg:grid-cols-4 p-3">
+        <div className="grid grid-cols-2 gap-2 xl:grid-cols-4 lg:grid-cols-4 mt-10">
           <div className="w-full h-auto bg-[#039AE4] rounded-[5px] xl:rounded-[20px]  text-[#ffffff] flex flex-col items-center justify-center py-3 px-1 ">
             <div className="size-8 bg-[#ffffff] flex items-center justify-center mb-3 rounded-full">
               < TbHeartHandshake className="size-6  text-[#039AE4]" />
@@ -34,9 +34,8 @@ const OurValues = () => {
             </p>
           </div>
   
-          <p className="hidden lg:block">
 
-          <div className="w-full h-auto bg-[#039AE4] rounded-[5px]  xl:rounded-[20px] text-[#ffffff] flex flex-col items-center justify-center  py-5 px-5">
+          <div className="w-full h-auto lg:bg-[#039AE4] rounded-[5px]  bg-[#121826]  xl:rounded-[20px] text-[#ffffff] flex flex-col items-center justify-center  py-2 px-3">
             <div className="size-8 bg-[#ffffff] flex items-center justify-center mb-3 rounded-full">
               <TbHeartHandshake className="size-6  text-[#039AE4]" />
             </div>
@@ -45,34 +44,10 @@ const OurValues = () => {
               We stay ahead by embracing the latest technologies and continuously improving our platform to offer faster, more efficient solutions for your financial needs.
             </p>
           </div>
-          </p>
 
-          <div className=" sm:block lg:hidden w-full h-auto bg-[#121826] rounded-[5px]  xl:rounded-[20px] text-[#ffffff] flex flex-col items-center justify-center  py-2 px-3">
+            <div className="  w-full h-auto bg-[#039AE4]  lg:bg-[#121826] rounded-[5px]  xl:rounded-[20px] text-[#ffffff] flex flex-col items-center justify-center  py-2  px-3">
             <div className="size-8 bg-[#ffffff] flex items-center justify-center mb-3 rounded-full">
               < TbHeartHandshake className="size-6 text-[#121826]" />
-            </div>
-            <h2 className="text-center text-[14px] xl:text-[18px] font-bold font-Inter leading-[21px] xl:leading-[27px] mb-2">Innovation</h2>
-            <p className="text-center text-[10px] xl:text-[14px] font-Inter leading-[15px] font-normal xl:leading-[21px]">
-              We stay ahead by embracing the latest technologies and continuously improving our platform to offer faster, more efficient solutions for your financial needs.
-            </p>
-          </div>
-
-          <p className="hidden lg:block">
-            <div className="  w-full h-auto bg-[#121826] rounded-[5px]  xl:rounded-[20px] text-[#ffffff] flex flex-col items-center justify-center  py-2  px-3">
-            <div className="size-8 bg-[#ffffff] flex items-center justify-center mb-3 rounded-full">
-              < TbHeartHandshake className="size-6 text-[#121826]" />
-            </div>
-            <h2 className="text-center text-[14px] xl:text-[18px] font-bold font-Inter leading-[21px] xl:leading-[27px] mb-2">Customer Focus</h2>
-            <p className="text-center text-[10px] xl:text-[14px] font-Inter leading-[15px] font-normal xl:leading-[21px]">
-              Your satisfaction is at the heart of everything we do. From personalized support to user-friendly features, we are dedicated to providing an exceptional experience for all our customers.
-            </p>
-          </div>
-          </p>
-
-
-          <div className=" sm:block lg:hidden w-full h-auto bg-[#039AE4] rounded-[5px]  xl:rounded-[20px] text-[#ffffff] flex flex-col items-center justify-center  py-2  px-3">
-            <div className="size-8 bg-[#ffffff] flex items-center justify-center mb-3 rounded-full">
-              < TbHeartHandshake className="size-6  text-[#039AE4] " />
             </div>
             <h2 className="text-center text-[14px] xl:text-[18px] font-bold font-Inter leading-[21px] xl:leading-[27px] mb-2">Customer Focus</h2>
             <p className="text-center text-[10px] xl:text-[14px] font-Inter leading-[15px] font-normal xl:leading-[21px]">
