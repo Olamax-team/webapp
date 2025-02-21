e7b99d8a109a82ea3f413306df2dd6b9201bdb43
debyrise <deborahoby693@gmail.com>
2025-02-21 12:03:11 +0100

    moduleOne

---
+++ src/components/educationalCenter/moduleOne.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const ModuleOne = () => {
+    return(
+        <React.Fragment>
+            <div></div>
+
+        </React.Fragment>
+
+    )
+}
+export default ModuleOne;
